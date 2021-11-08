d6436186c3515851338b54441f1e4c19e3a6abe8
Rafeeq <87922252+Pulapatta@users.noreply.github.com>
2021-11-08 19:19:36 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -35,7 +35,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
 default_start_msg = """
- ꜱᴏᴏʀy ᴅᴜᴅᴇ....❗️❗️\n \nɪ ᴀᴍ ᴡᴏʀᴋ ᴏɴʟʏ ꜰᴏʀ ᴍʏ ʙᴏꜱꜱ\n \n ᴅᴏ ɴᴏᴛ ꜰʟᴏᴏᴅ ᴍᴇ ɪ ᴅᴏɴ'ᴛ ʟɪᴋᴇ ꜰʟᴏᴏᴅɪɴɢ\n \nᴅᴏ ɴᴏᴛ ᴡᴀꜱᴛᴇ ᴛɪᴍᴇ ɢᴏ ᴀʜᴇᴀᴅ **𝐁𝐈𝐓𝐂𝐇**\n \n \n**➪ Developer : 𝓐𝓷𝓪𝓷𝓭𝓱𝓾**
+ ꜱᴏᴏʀy ᴅᴜᴅᴇ....❗️❗️\n \nɪ ᴀᴍ ᴡᴏʀᴋ ᴏɴʟʏ ꜰᴏʀ ᴍʏ ʙᴏꜱꜱ\n \n ᴅᴏ ɴᴏᴛ ꜰʟᴏᴏᴅ ᴍᴇ ɪ ᴅᴏɴ'ᴛ ʟɪᴋᴇ ꜰʟᴏᴏᴅɪɴɢ\n \nᴅᴏ ɴᴏᴛ ᴡᴀꜱᴛᴇ ᴛɪᴍᴇ ɢᴏ ᴀʜᴇᴀᴅ **𝐁𝐈𝐓𝐂𝐇**\n \n \n**➪ Developer : 𝑅𝒶𝒻𝑒𝑒𝓆**
 
 """
 START_MSG = environ.get('START_MSG', default_start_msg)
