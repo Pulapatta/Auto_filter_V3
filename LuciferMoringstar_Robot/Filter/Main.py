aafac8eaac830560c9f4078be21b43d3508f6bb7
Rafeeq <87922252+Pulapatta@users.noreply.github.com>
2021-11-11 13:01:42 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -376,4 +376,5 @@ async def cb_handler(client: Client, query: CallbackQuery):
     else:
         
         
-        await query.answer("hi /nbro",show_alert=True)
+        
+        await query.answer("കൌതുകും ലേശം കൂടുതൽ ആണല്ലേ👀",show_alert=True)
